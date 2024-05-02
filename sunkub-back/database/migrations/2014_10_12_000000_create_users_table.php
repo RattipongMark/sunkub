c3529859c861367934a5150e0b4978a708c32130
RattipongMark <98859091+RattipongMark@users.noreply.github.com>
2024-05-02 19:49:55 +0700

    Revert "auth1"
    This reverts commit 19061e398d55f6aac79786ce86736d8a7d1f1a5f.

--- sunkub-back/database/migrations/2014_10_12_000000_create_users_table.php
+++ sunkub-back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fname')->nullable();
-            $table->string('lname')->nullable();
-            $table->enum('gender', ['male', 'female', 'other'])->nullable();
-            $table->date('dob');
+            $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
