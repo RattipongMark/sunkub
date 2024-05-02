94d9028a991e00a2577f0910fc65b87496f8cadc
RattipongMark <98859091+RattipongMark@users.noreply.github.com>
2024-05-02 20:36:58 +0700

    auth+daidy

--- sunkub-back/app/Models/User.php
+++ sunkub-back/app/Models/User.php
@@ -18,7 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fname',
+        'lname',
+        'gender',
+        'dob',
         'email',
         'password',
     ];
