dd6a21120765c9e35e0289c611d62ddaa8f0e8a2
RattipongMark <98859091+RattipongMark@users.noreply.github.com>
2024-05-04 00:13:04 +0700

    Reapply "landing"
    This reverts commit c8279249417e10924d599c366086e6f7c97068d7.

---
+++ sunkub-back/database/migrations/2024_05_03_200804_create_ports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ports', function (Blueprint $table) {
+            $table->id('port_id');
+            $table->unsignedBigInteger('user_id'); 
+            $table->unsignedBigInteger('broker_id');
+            $table->string('user_broker');
+            $table->string('password'); 
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('broker_id')->references('broker_id')->on('brokers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ports');
+    }
+};
---
+++ sunkub-back/resources/views/navbar.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') | sunkub</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/css/navbar.css">
+    <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
+    <style>
+      body {
+          font-family: 'Kanit';
+      }
+      </style>
+</head>
+<body>
+    <div class="navbar bg-base-100 navblur">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl"><img src="{{url('images/LOGO.svg')}}" alt=""></a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1">
+            <li class="purple200"><a>เข้าสู่ระบบ</a></li>
+            <li><a>ลงทะเบียน</a></li>
+          </ul>
+        </div>
+    </div>
+
+    <div>
+        @yield('content')
+    </div>
+</body>
+</html>
