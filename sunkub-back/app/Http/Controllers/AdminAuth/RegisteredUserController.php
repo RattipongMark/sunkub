d2f75e007cb1a63c7ecba79b934a7190f1ca2775
RattipongMark <98859091+RattipongMark@users.noreply.github.com>
2024-05-03 19:23:49 +0700

    login_admin_user

---
+++ sunkub-back/app/Http/Controllers/AdminAuth/RegisteredUserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\AdminAuth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            "fname" => "required",
+            "lname" => "required",
+            "gender" => "required",
+            "dob" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed", 
+        ]);
+
+        $user = User::create([
+            'fname' => $request['fname'],
+            'lname' => $request['lname'],
+            'gender' => $request['gender'],
+            'dob' => $request['dob'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
--- sunkub-back/routes/web.php
+++ sunkub-back/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Auth::routes();
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
+
+require __DIR__.'/adminauth.php';
